5a5e681786059f4e5fe0fbb4e246b31007f89db2
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-08-21 17:12:41 +0530

    setup tailwind css for better practice

--- src/components/Introduction/Intro.jsx
+++ src/components/Introduction/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Intro = () => {
   return (
       <>
-          <h1>Introduction to React</h1>
+        <h1 className='text-center'>Introduction to React</h1>
         <p>Learning react from scratch and build the foundation</p>
       </>
   )
