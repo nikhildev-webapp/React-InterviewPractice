efd13c4b737076423ab734e2529eca8ac0d12d10
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-09-05 15:55:24 +0530

    project -gitub user finder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import GithubUser from "./Projects/GithubUser_Finder/GithubUser"
 import UserProfiles from "./Projects/UserProfile/UserProfiles"
 
 function App() {
   
   return (
     <>
-        <UserProfiles/>
+        <GithubUser/>
     </>
     
   )
